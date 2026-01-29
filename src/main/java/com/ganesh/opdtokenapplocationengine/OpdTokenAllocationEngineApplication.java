d07738b2cbbc36430031b28016c57de830354542
Sai Ganesh <ganesh.12222126@lpu.in>
2026-01-29 14:53:12 +0530

    Initial commit

---
+++ src/main/java/com/ganesh/opdtokenapplocationengine/OpdTokenAllocationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh.opdtokenapplocationengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpdTokenAllocationEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpdTokenAllocationEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ganesh/opdtokenapplocationengine/OpdTokenApplocationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganesh.opdtokenapplocationengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpdTokenApplocationEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
