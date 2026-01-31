3e057bd7470f30718cbe5126a6d611f5e442eac0
Sai Ganesh <ganesh.12222126@lpu.in>
2026-01-31 11:39:18 +0530

    Creating a TokenRequest for values

---
+++ src/main/java/com/ganesh/opdtokenapplocationengine/model/TokenRequest.java
@@ -0,0 +1,33 @@
+package com.ganesh.opdtokenapplocationengine.model;
+
+public class TokenRequest {
+    private String patientId;
+
+    public BookingType getBookingType() {
+        return bookingType;
+    }
+
+    public void setBookingType(BookingType bookingType) {
+        this.bookingType = bookingType;
+    }
+
+    public String getSlotId() {
+        return slotId;
+    }
+
+    public void setSlotId(String slotId) {
+        this.slotId = slotId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    private  BookingType bookingType;
+    private String slotId;
+
+}
